e214fd0d38e8c9a40676835d70a35ef72ffb69fb
charosanchezinda-spec <charosanchezinda@gmail.com>
2025-11-19 19:49:30 -0300

    Add monthly window column to electoral tracking

--- tracking_electoral.py
+++ tracking_electoral.py
@@ -144,6 +144,7 @@ df
 df = df.sort_values('fecha')
 df['Ventana_D'] = df['fecha']
 df['Ventana_S'] = df['fecha'].dt.to_period('W')
+df['Ventana_M'] = df['fecha'].dt.to_period('M')
 
 # %%
 #Quinto Paso: pesos por ventanas
